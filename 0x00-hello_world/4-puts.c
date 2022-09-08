3ae317f1035d035d23d383a280edf70c388ce328
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-08 16:07:31 -0700

    script that prints a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-\* print an output *\ 
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }

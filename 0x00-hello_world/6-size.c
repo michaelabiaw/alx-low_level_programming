f81721a11148f2bfc818365a1c4b8a29c6363d1d
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-09 17:46:11 -0700

    Add sizesof various types on computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,7 @@
  */
 int main(void)
 {
+
 	char c;
 	int i;
 	long int li;
@@ -16,7 +17,7 @@ int main(void)
 	printf("Size of char: %lu bytes\n", sizeof(c));
 	printf("Size of int: %lu bytes\n", sizeof(i));
 	printf("Size of long int: %lu bytes\n", sizeof(li));
-	printf("Size of long long int: %lu byte\n", sizeof(lli))
+	printf("Size of long long int: %lu byte\n", sizeof(lli));
 	printf("Size of float: %zu byte\n", sizeof(f));
 	return (0);
 }

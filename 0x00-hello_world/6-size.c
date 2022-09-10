9eb4717d1417c76e3586eead34aa77fdafd41fa6
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-09 17:39:18 -0700

    Add sizesof various types on computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-int main(void) 
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char c;
 	int i;
 	long int li;
 	long long int lli;
 	float f;
-   
-
-    /* sizeof evaluates the size of a variable */
     printf("Size of char: %lu bytes\n", sizeof(c));
     printf("Size of int: %lu bytes\n", sizeof(i));
     printf("Size of long int: %lu bytes\n", sizeof(li));

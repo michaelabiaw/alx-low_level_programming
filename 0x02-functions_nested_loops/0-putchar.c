fa1316ef9d923b72fa931223bee2d6d2d4b42987
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-14 16:59:56 -0700

    print _putchar to the output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,16 @@
  */
 int main(void)
 {
-	char *ch = "Hello";
+	_putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
 
-	while (*ch)
-	{
-		_putchar(*ch);
-		ch++;
-	}
-	_putchar('\n');
 
 	return (0);
 }

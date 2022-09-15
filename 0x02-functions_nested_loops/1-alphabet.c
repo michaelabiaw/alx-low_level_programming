e7fdb1eebb7d25daa01d2ea8c974da248962e056
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-14 17:21:04 -0700

    print lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char b;
+	for(b = 'e', b <= 't', b++)
+	{
+		_putchar(b);
+	}
+_putchar('\n');
+}

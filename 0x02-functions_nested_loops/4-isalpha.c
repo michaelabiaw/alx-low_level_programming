1013a95b1af623eac7eb2dc9283c9f357c7265d6
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-14 19:04:37 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check alphabet character
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if ((b >= 'a' && b <= 't') || (b >= 'A' && b <= 'T'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

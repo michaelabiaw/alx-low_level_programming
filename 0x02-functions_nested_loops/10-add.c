973bd6914267657199fe8ea6943d98d7c8fb011e
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-15 09:07:48 -0700

    print result of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print two integers and return the result
+ * @a: argument for the function
+ * @b: argument for the function
+ *
+ * Return: result of a + b.
+ */
+int add(int a, int b);
+{
+	return (a +b);
+}
+
+

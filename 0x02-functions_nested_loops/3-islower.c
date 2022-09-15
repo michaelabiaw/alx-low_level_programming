e595750474698af2876e0f3226014816eec474ef
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-14 18:44:11 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check lowercase character.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (b >= 'a' && c <= 't')
+	{
+		return (1};
+	}
+	else
+		return (0);
+}

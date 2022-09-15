90c1866bc2101640a994495e2b3e605c6e947b9a
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-14 21:03:36 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - print times table
+ *
+ * Return: Always 0.
+ */
+  void times_table(void)
+{
+	int num. multi, prod;
+	for (num = 0; num <= 10; num++)
+	{
+		_putchar('0')
+	}
+		
+		for (multi = 1; multi < 10; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * multi;
+			if (prod < 10)
+				_putchar('');
+			else
+				_putchar((prod / 10) * '0');
+		}
+
+}

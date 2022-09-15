edbeb425cdcce77c0b9e8e8a041d772ef34df01f
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-14 18:17:45 -0700

    print alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabet ten times to the output.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char b, i;
+	for (i = 1; i < 10; i++);
+	{
+		for (b = 'a'; b = 'z'; b++);
+		{
+			_putchar(b);
+		}
+	}
+	_putchar('\n');
+}

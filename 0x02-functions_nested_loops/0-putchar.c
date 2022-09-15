1cb6d503a310b06de5abf1453c3f3d8c9c0bf744
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-14 17:57:57 -0700

    print lowercase

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-
 	_putchar('_');
         _putchar('p');
         _putchar('u');
@@ -17,6 +16,5 @@ int main(void)
         _putchar('a');
         _putchar('r');
         _putchar('\n');
-
 	return(0);
 }

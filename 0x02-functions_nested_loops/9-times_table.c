2cfc24a6912af39b105057fe19dd3d2d7326fc8f
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-15 08:55:26 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,21 +7,24 @@
   void times_table(void)
 {
 	int num, multi, prod;
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0')
+		_putchar('0');
 	}
 		
-		for (multi = 1; multi < 10; multi++)
+		for (multi = 1; multi <= 9; multi++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			prod = num * multi;
-			if (prod < 10)
+			if (prod <= 9)
 				_putchar('');
 			else
 				_putchar((prod / 10) * '0');
+			_putchar((prod % 10) * '0');
 		}
+		_putchar('\n');
+
 
 }

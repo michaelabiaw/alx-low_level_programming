5cc8afed78e57a90f04f33ec0993b0c40f45a8cd
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-16 16:21:54 -0700

    display uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ * _isupper - checks if uppercase
+ * @c: character to check
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme file for more function, more nested loops

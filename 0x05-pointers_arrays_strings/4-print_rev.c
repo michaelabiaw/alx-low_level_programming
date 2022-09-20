229c8956905665ac47ad92604417110ecb5273e4
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-20 14:36:34 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 /**
- * print_rev - prints a string, in reverse, followed by a new line
+ *print_rev - prints a string, in reverse, followed by a new line
  *
- * @str: string
+ *@str: string
  * Return: Always 0.
  */
 

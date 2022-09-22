1d6e4090dda53d433d6a806ca52a29e6a07a7a5f
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-22 14:09:14 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,6 +14,13 @@ char *_strcat(char *dest, char *src)
 {
 	char s1[98] = "Hello ";
 	char s2[] = "World!";
+	char *ptr = dest;
+
+	while (*ptr != 0)
+		ptr++;
+	while (*src != 0)
+		*ptr++ = *src++;
+	*ptr = 0;
 	
 	/* concateneate char dest, cha src */
 	strcat(s1, s2);

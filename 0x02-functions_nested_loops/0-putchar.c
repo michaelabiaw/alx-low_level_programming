5e72f60db3d79e99a63f991eec1de2c3d42446c6
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-23 16:46:42 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * main - print _putchar followed by  a new line
+ * print_alphabet - print _putchar followed by  a new line
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	_putchar('_');

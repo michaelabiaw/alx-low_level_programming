9dd77c72371a62f5794ec65a29a740e8d828ae67
michaelabiaw <michaelabiaw@gbiaw@gmail.com>
2022-09-29 13:35:11 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,13 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		 _putchar('\n');
 	}
 
 	else
-		_putchar('\n');
+	{
+		_putchar(*s);
+    		_puts_recursion(s + 1);
+	}
 }
 
